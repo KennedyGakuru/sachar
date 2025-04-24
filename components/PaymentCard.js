5d06938a12c8d99da6b5d69c3ed92e10380cf876
KenGakuru2 <kennedygakuru3@gmail.com>
2025-04-24 09:26:34 -0400

    Refactored Payment component: -Removed `selectedMethod`state -Replaced with `isSelected` and `onSelected` props Improved code structure for handling selection

--- components/PaymentCard.js
+++ components/PaymentCard.js
@@ -3,19 +3,15 @@ import { Ionicons } from '@expo/vector-icons';
 import { useState } from 'react';
 
 const PaymentsCard = ({title,imageSource, isSelected, onSelect}) => {
-    const [selectedMethod, setSelectedMethod] = useState(null);
-    const handlePress = () =>{
-        setSelectedMethod(title);
-    };
     return(
         <TouchableOpacity   style={{height: 50, width: 350, borderRadius: 10,
-            backgroundColor: selectedMethod === title ? '#4CAF50' : '#CCFFB3', 
+            backgroundColor: isSelected ? '#4CAF50' : '#CCFFB3', 
             flexDirection: 'row',
             justifyContent: 'space-between',
             alignItems: 'center',
             padding: 8,
-          }} onPress={onSelect}>
-           <Ionicons name='checkmark-done-circle' size={24} color={selectedMethod === title ? 'green':'white'} />
+          }} onPress={() => onSelect(title)}>
+           <Ionicons name='checkmark-done-circle' size={24} color={isSelected ? 'green':'white'} />
            <Text className="text-[20px]">{title}</Text> 
            <Image source={imageSource}
            className='h-[50px] w-[50px]' />
