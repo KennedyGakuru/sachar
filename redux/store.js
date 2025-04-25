b8fa80279e7c94596fd5783f50e7cfed29827c01
KenGakuru2 <kennedygakuru3@gmail.com>
2025-04-25 15:13:35 -0400

    Register checkoutSlice in store.js

--- redux/store.js
+++ redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from './cartSlice'
 import favoriteReducer from './favoriteSlice'
+import checkoutReducer from './checkoutSlice'
 
 const store = configureStore({
     reducer: {
         cart: cartReducer,
         favorite: favoriteReducer,
+        checkout: checkoutReducer,
     },
 });
 
