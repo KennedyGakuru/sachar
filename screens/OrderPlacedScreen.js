20d4a79aa6d2feb55558c29a2c25b0ff7cfaecd3
KenGakuru2 <kennedygakuru3@gmail.com>
2025-04-25 15:20:32 -0400

    Update text color for dark mode in OrderPlaced screen

--- screens/OrderPlacedScreen.js
+++ screens/OrderPlacedScreen.js
@@ -12,11 +12,12 @@ const OrderPlacedScreen = () => {
             <View className="flex-1 items-center justify-center">
                 <Image source={require('../assets/icegif-310.gif')}
                 className="h-[150px] w-[250px]"/>
-            <Text className="text-4xl text-bold mt-10">Congratulations</Text>
-            <Text>Order has been placed. Your order no. is #1737</Text>
+            <Text className={`text-4xl text-bold mt-10 ${darkMode ? 'text-white' : 'text-black'}`}>Congratulations</Text>
+            <Text className={`${darkMode ? 'text-white': 'text-black'}`}>Order has been placed. Your order no. is #1737</Text>
             </View>
             <View className="items-center">
-            <TouchableOpacity onPress={() => navigation.navigate('HomeTabs',{screen:"Home"})} className="bg-[#75F94C] h-[65px] w-[350px] rounded-[20px] justify-center items-center">
+            <TouchableOpacity onPress={() => navigation.navigate('HomeTabs',{screen:"Home"})}
+             className="bg-[#75F94C] h-[65px] w-[350px] rounded-[20px] justify-center items-center">
                 <Text className="text-3xl text-bold text-white">Go Home</Text>
             </TouchableOpacity>
             </View>
